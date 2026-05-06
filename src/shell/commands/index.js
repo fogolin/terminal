06f0bbd1d58a6906e52fc61d993eaa02bbf1c6dd
Lucas Fogolin <lucas@fogol.in>
2026-05-06 16:20:05 -0300

    ✨ Added ctx.openPanel to the terminal.

--- src/shell/commands/index.js
+++ src/shell/commands/index.js
@@ -1,28 +1,29 @@
-import clear   from './clear.js';
-import help    from './help.js';
-import print   from './print.js';
-import whoami  from './whoami.js';
-import pwd     from './pwd.js';
-import ls      from './ls.js';
-import cd      from './cd.js';
-import cat     from './cat.js';
-import tail    from './tail.js';
-import touch   from './touch.js';
-import mkdir   from './mkdir.js';
-import rm      from './rm.js';
-import cp      from './cp.js';
-import mv      from './mv.js';
-import find    from './find.js';
-import grep    from './grep.js';
-import su      from './su.js';
-import ping    from './ping.js';
-import curl    from './curl.js';
-import top     from './top.js';
-import nano    from './nano.js';
-import theme   from './theme.js';
-import man     from './man.js';
-import apt     from './apt.js';
-import calc    from './calc.js';
+import clear from './clear.js';
+import help from './help.js';
+import print from './print.js';
+import whoami from './whoami.js';
+import pwd from './pwd.js';
+import ls from './ls.js';
+import cd from './cd.js';
+import cat from './cat.js';
+import tail from './tail.js';
+import touch from './touch.js';
+import mkdir from './mkdir.js';
+import rm from './rm.js';
+import cp from './cp.js';
+import mv from './mv.js';
+import find from './find.js';
+import grep from './grep.js';
+import su from './su.js';
+import ping from './ping.js';
+import curl from './curl.js';
+import top from './top.js';
+import nano from './nano.js';
+import theme from './theme.js';
+import man from './man.js';
+import apt from './apt.js';
+import calc from './calc.js';
+import testpanel from './testpanel.js';
 
 const ALL_COMMANDS = [
     clear, help, print,
@@ -31,7 +32,7 @@ const ALL_COMMANDS = [
     touch, mkdir, rm, cp, mv,
     find, grep,
     su, ping, curl, top, nano, theme, man,
-    apt, calc,
+    apt, calc, testpanel
 ];
 
 const registry = new Map();
@@ -43,6 +44,6 @@ ALL_COMMANDS.forEach(cmd => {
 
 // Inject registry into help/man for introspection without circular import
 help._registry = registry;
-man._registry  = registry;
+man._registry = registry;
 
 export default registry;
